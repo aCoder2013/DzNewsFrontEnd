758254c56ea5d6d8a8c277c075670977a838ea9a
aCoder2013 <247608980@qq.com>
2015-10-25 21:04:36 +0800

    支持分页

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,9 +4,27 @@
 var newsControllers = angular.module('newsControllers', []);
 
 newsControllers.controller('listController', function ($scope,$http) {
+    $scope.page = 0;
     $http.get('/api/news').success(function(data) {
         $scope.newses = data;
     });
+
+    $scope.lastPage = function(){
+      $scope.page = $scope.page-1;
+      $http.get('/api/news?page='+$scope.page).success(function(data){
+          $scope.newses = data ;
+      });
+    };
+
+    $scope.nextPage = function(){
+      $scope.page = $scope.page+1;
+      $http.get('/api/news?page='+$scope.page).success(function(data){
+          // $scope.newses =  data;
+          angular.forEach(data,function(item) {
+               $scope.newses.push(item);
+          });
+      });
+    };
 });
 
 newsControllers.controller("newsDetailCtrl",['$scope', '$routeParams','$http','$sce',
