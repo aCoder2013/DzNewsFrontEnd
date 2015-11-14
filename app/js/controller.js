e39bb839b8cdb44b5a81246ed3e528b2be8d76b9
aCoder2013 <247608980@qq.com>
2015-11-14 10:34:54 +0800

    支持提交评论

--- app/js/controller.js
+++ app/js/controller.js
@@ -35,26 +35,35 @@ newsControllers.controller('listController', function ($scope,$http,instance) {
 
 
 newsControllers.controller("newsDetailCtrl",function($scope, $routeParams,$http,$sce,instance) {
-          $scope.formContent;
-          $scope.pageTitle;
-          $scope.detail_url;
           if(!(instance.url)){
+            //如果是直接通过url访问，则通过newsitem.id获取相应的新闻详情URL
             $http.get("/api/news/"+$routeParams.id).success(function (data) {
             instance.url = data._links.detail.href;
             $http.get(instance.url).success(function (data) {
               $scope.detail = data.detail ;
               $scope.detail.content = $sce.trustAsHtml($scope.detail.content);
-              $scope.actionUrl = $sce.trustAsResourceUrl('/api/detail/'+$scope.detail.id+'/comment/new');
               $scope.pageTitle = $scope.detail.title;
               });
             });
           }else {
+            //如果是通过首页跳转访问则直接获取新闻详情
             $http.get(instance.url).success(function (data) {
               $scope.detail = data.detail ;
               $scope.detail.content = $sce.trustAsHtml($scope.detail.content);
               $scope.actionUrl = $sce.trustAsResourceUrl('/api/detail/'+$scope.detail.id+'/comment/new');
               $scope.pageTitle = $scope.detail.title;
-          });
-          }
+            });
+          };
 
+          $scope.submitComment = function (comment) {
+              $http.post(instance.url+'/comment/new', $.param({
+                name:comment.name,
+                email:comment.email,
+                content:comment.content
+              }),{
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+              }).success(function (data) {
+                  window.alert(instance.url);
+              });
+          }
 });
